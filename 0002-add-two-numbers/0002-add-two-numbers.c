03d9545032926fcb2776866067e271ea9e6b19b0
varunimanhas08 <varuni.manhas2025@vitstudent.ac.in>
2026-07-29 11:02:30 +0530

    Time: 2 ms (40.82%), Space: 13.4 MB (85.22%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -0,0 +1,44 @@
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) 
+{
+    struct ListNode *head = NULL, *temp = NULL;
+    int carry = 0;
+
+    while(l1 != NULL || l2 != NULL || carry != 0)
+    {
+        int sum = carry;
+
+        if(l1 != NULL)
+        {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if(l2 != NULL)
+        {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+
+        struct ListNode* newnode =
+            (struct ListNode*)malloc(sizeof(struct ListNode));
+
+        newnode->val = sum % 10;
+        newnode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    return head;
+}
