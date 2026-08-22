fa962cc28c0a7a928820911e2f91ceb0fa58e0ad
varunimanhas08 <varuni.manhas2025@vitstudent.ac.in>
2026-08-22 21:39:27 +0530

    Time: 148 ms (5.1%), Space: 89.5 MB (8%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        set<int> s;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) != s.end()) {
+                return nums[i];
+            }
+
+            s.insert(nums[i]);
+        }
+
+        return -1;
+    }
+};
