68c7923bb589b800480be9cc06ba51e82ae1024c
varunimanhas08 <varuni.manhas2025@vitstudent.ac.in>
2026-09-04 23:52:38 +0530

    Time: 0 ms (100%), Space: 31.1 MB (66.39%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> suffixMin(n);
+
+        // Find suffix minimum
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(suffixMin[i + 1], nums[i]);
+        }
+
+        // Keep track of prefix maximum
+        int prefixMax = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            prefixMax = max(prefixMax, nums[i]);
+
+            if (prefixMax - suffixMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
